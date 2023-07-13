0caf6967f9d3e858f1eee230deed5e594d5dae2d
squirre17 <3319277663@qq.com>
2023-07-13 18:50:55 +0800

    complete prim

--- inc/prim.h
+++ inc/prim.h
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "graph.h"
 
+/// @brief 创建两个集合 一个是加入的 一个是未加入的 以结点进行划分 每次从未加入的结点中选出一个 它的距离到已加入的所有结点的距离最小
+///        然后把它加入 已加入的集合
 class Prim {
   public:
     Graph graph;
